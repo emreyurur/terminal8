a5ed5f818056053e88e0fcb9174cab790563e593
K0twHan <ibrahim.batuhan.tekin.0742@gmail.com>
2026-09-25 19:52:41 +0300

    test for mainnet variables

--- src/modules/oracle/oracle.service.ts
+++ src/modules/oracle/oracle.service.ts
@@ -247,7 +247,7 @@ export class OracleService implements OnModuleInit {
   async getSupportedAssets(): Promise<OracleAsset[]> {
     return [
       { code: "XLM", issuer: null },
-      { code: "USDC", issuer: "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5" }, // testnet
+      { code: "USDC", issuer: "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN" }, // mainnet (Centre/Circle)
     ];
   }
 
